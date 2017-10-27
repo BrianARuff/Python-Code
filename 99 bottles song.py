142662f5ae531e86dc7140b27562ed3b95ace168
Brian Ruff <thelonewolf198927@gmail.com>
2017-10-27 10:46:17 -0400

    Python 99 bottles and hello world app

---
+++ 99 bottles song.py
@@ -0,0 +1,9 @@
+for i in range(99,0,-1):
+    if i == 1:
+        print(i, "bottle of beer on the wall", i, "bottle of beer.")
+        print("Take one down, pass it around.\n")
+        print(i-1, "bottles of beer on the wall.")
+        print("Now go home and don't drive while drunk.")
+    else:
+        print(i, "bottles of beer on the wall", i, "bottles of beer.")
+        print("Take one down, pass it around\n")
